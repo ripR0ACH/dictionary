02b516c611893b45820d480542ce292907875e1e
zach morrison <morrisonizach@gmail.com>
2022-02-26 05:43:23 -0600

    made a web scraper for collecting every word in the dictionary, tomorrow I will update it to record their definitions

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
+	alph := [27]string{"0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 	data := "data.csv"
 	file, err := os.Create(data)
 	if err != nil {
@@ -22,21 +24,28 @@ func main() {
 	defer writer.Flush()
 
 	c := colly.NewCollector()
-	fmt.Println("scrape starting...\nscraper stats:\n\t", c)
-	c.OnHTML("*", func(element *colly.HTMLElement) {
-		// element.ForEach("li", func(_ int, el *colly.HTMLElement) {
-		// 	writer.Write([]string{el.ChildText("a:nth=child(1)")})
-		// })
-		// fmt.Println("\nscrape done :)\n")
-		fmt.Println(element)
-	})
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	fmt.Println("scrape starting...\n")
+	c.OnHTML("main ul", func(e *colly.HTMLElement) {
+		e.ForEach("li", func(_ int, el *colly.HTMLElement) {
+			writer.Write([]string{el.ChildText("a:first-child")})
+		})
+		fmt.Println("\nscrape done :)\n")
 	})
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
-	c.Visit("https://dictionary.com/")
-	fmt.Println("scraper stats:\n\t", c)
+	scraping := true
+	c.OnError(func(r *colly.Response, err error) {
+		print(r)
+		scraping = false
+	})
+	for i := 0; i < len(alph); i++ {
+		ind := 1
+		for scraping {
+			c.Visit("https://dictionary.com/list/" + alph[i] + "/" + strconv.Itoa(ind))
+			ind += 1
+		}
+		scraping = true
+	}
 	fmt.Println("program ending...")
 }
